4f8a1f6b421b13ac466bfd113a0d815312580526
Jeremy <jeremy.fardilha@centrale-marseille.fr>
2019-11-19 15:10:51 +0100

    début database

---
+++ database.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import geocode as geo
+
+
+def ouverture_bdd(bdd):
+    return pd.read_csv(bdd)
+
+
+def ajout_ligne(bdd, url, adresse):
+    db = ouverture_bdd(bdd)
+    l = len(db)
+    db.loc[l] = [l+1, adresse, geo.geocode(adresse)[0], geo.geocode(adresse)[1], [], url]
+    db.to_csv(bdd, index=False)
+    return None
+
+
+def affiche_bdd(bdd):
+    return ouverture_bdd(bdd)
+
+
+def get_coordonnees_url(bdd, url):
+    db = ouverture_bdd(bdd)
+    i = 0
+    url_found = False
+    while not url_found:
+        if db.loc[i].lien_artété != url:
+            i += 1
+        else:
+            return db.loc[i].lon, db.loc[i].lat
+
+
+def get_coordonnées_adresse(bdd, adresse):
+    db = ouverture_bdd(bdd)
+    i = 0
+    adresse_found = False
+    while not adresse_found:
+        if db.loc[i].adresse != adresse:
+            i += 1
+        else:
+            return db.loc[i].lon, db.loc[i].lat
+
+
+def get_url_adresse(bdd, adresse):
+    db = ouverture_bdd(bdd)
+    i = 0
+    adresse_found = False
+    while not adresse_found:
+        if db.loc[i].adresse != adresse:
+            i += 1
+        else:
+            return db.loc[i].url

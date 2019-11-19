b020a3c2f2b273a52a3b8bedc998f531bd72b455
Jeremy <jeremy.fardilha@centrale-marseille.fr>
2019-11-19 17:07:54 +0100

    classification des lieux et pathologies

--- database.py
+++ database.py
@@ -2,6 +2,10 @@ import pandas as pd
 import geocode as geo
 
 
+#liste des classifications de pathologies
+##'classification_pathologies'=["affaissemnt", "altération", "risque de chutes", "fissures", "fissures", "débris", "affaissement", "basculement d'escalier", "risque d'effondrement"]
+
+
 def ouverture_bdd(bdd):
     return pd.read_csv(bdd)
 
--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import geocode as geo
 import carte
 import database
 
-# adresse1 = geo.geocode(geo.adresse_test)
-# # adresse_centrale = geo.geocode("38 rue frédéric joliot curie marseille")
-# # c = carte.creation_carte()
-# # carte.creation_marker(c, adresse1[0], adresse1[1], "ca a marché")
-# # carte.creation_marker(c, adresse_centrale[0], adresse_centrale[1], "Centrale Marseille")
-# #
-# # c.save('carte.html')
+adresse1 = geo.geocode(geo.adresse_test)
+adresse_centrale = geo.geocode("38 rue frédéric joliot curie marseille")
+c = carte.creation_carte()
+carte.creation_marker(c, adresse1[0], adresse1[1], "ca a marché")
+carte.creation_marker(c, adresse_centrale[0], adresse_centrale[1], "Centrale Marseille")
 
+c.save('carte.html')
 
-database.ajout_ligne("arretes.csv", "fsvdd", "41 rue de Rome Marseille", "affaissement caractéristique plancher bas du 2nd étage")

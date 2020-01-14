8fbd621167acb6dceae862da973547f0ae89f5ce
maelle.berthier <maelle.berthier@centrale-marseille.fr>
2020-01-14 18:54:17 +0100

    noouveau fichier récupération

--- geocode.py
+++ geocode.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 url_geocode = "http://api-adresse.data.gouv.fr/search/"
 adresse_test = "11 rue Moustier Marseille"
@@ -11,17 +10,3 @@ def geocode(adresse):
     lat = reponse.json().get("features")[0].get("geometry").get("coordinates")[1]
     return lat, lon
 
-def recup_adresse(texte):
-    fichier = open(texte, "r", encoding = "latin-1")
-    res = str(fichier.read()).partition("immeuble sis ")[2].partition("-")[0].partition("")[0]
-    return res
-
-def recup_pathologie(texte):
-    fichier = open(texte, "r", encoding = "latin-1")
-    res = str(fichier.read()).partition("pathologies suivantes :")[2].partition("Considérant")[0]
-    return res
-
-##Il reste à gérer les bas de page
-
-for i in os.listdir("./Datas/TXT"):
-    print(recup_pathologie("./Datas/TXT/" + i))

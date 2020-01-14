5061bf390386b3c3981fa71ae3a4557d9f1c00f6
maelle.berthier <maelle.berthier@centrale-marseille.fr>
2020-01-14 18:20:24 +0100

    récupération adresse

--- geocode.py
+++ geocode.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 url_geocode = "http://api-adresse.data.gouv.fr/search/"
 adresse_test = "11 rue Moustier Marseille"
@@ -9,5 +10,11 @@ def geocode(adresse):
     lon = reponse.json().get("features")[0].get("geometry").get("coordinates")[0]
     lat = reponse.json().get("features")[0].get("geometry").get("coordinates")[1]
     return lat, lon
+
+def recup_adresse(texte):
+    fichier = open(texte, "r", encoding = "latin-1")
+    res = str(fichier.read()).partition("immeuble sis ")[2].partition("-")[0].partition("")[0]
+    return res
+
 
 

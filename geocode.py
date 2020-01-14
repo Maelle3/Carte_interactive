e3775b993760caa074e15d38adcbb5077f9d2ea0
maelle.berthier <maelle.berthier@centrale-marseille.fr>
2020-01-14 18:50:53 +0100

    récupération pathologie (début car problème de bas de page)

--- geocode.py
+++ geocode.py
@@ -16,5 +16,12 @@ def recup_adresse(texte):
     res = str(fichier.read()).partition("immeuble sis ")[2].partition("-")[0].partition("")[0]
     return res
 
+def recup_pathologie(texte):
+    fichier = open(texte, "r", encoding = "latin-1")
+    res = str(fichier.read()).partition("pathologies suivantes :")[2].partition("Considérant")[0]
+    return res
 
+##Il reste à gérer les bas de page
 
+for i in os.listdir("./Datas/TXT"):
+    print(recup_pathologie("./Datas/TXT/" + i))
